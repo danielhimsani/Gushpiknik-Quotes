4c6d50e170f90c4e1d6be44553ad70bad4f1621c
ilayadmoni <ilayadmoni9@gmail.com>
2024-01-07 15:23:58 +0200

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,17 +13,38 @@ const App = () => {
     alignItems: 'center',
     justifyContent: 'center',
   };
+
   const headingStyles = {
-    color: 'black', // Set text color to white
+    color: 'black', 
     position: 'absolute',
     backgroundColor: 'rgb(204, 102, 0)',
-    top: 0,
+    top: -14,
     left: 0,
-    //width: '100%', // Set the width of the object
-    fontSize: '20px', // Set the font size of the text
+    display: 'flex',
+    fontSize: '20px', 
+    padding: '20px',
+    cursor: 'pointer',
+    marginRight: '10px',// Set the font size of the text
   };
+
+  const containerStyle = {
+    display: 'flex',
+    height: '100vh', // 100% of the viewport height
+    background: 'orange', // Background color for the entire screen
+    height: '100vh', 
+    width: "100vw",
+    flexDirection: 'column',
+  };
+
+  const contentStyle = {
+    padding: '30px',
+    background: 'rgb(255, 128, 0)', // Background color for the content area
+    alignItems: 'flex-start'
+    
+  };
+  
   const tabsContainerStyles = {
-    position: 'absolute',
+   position: 'absolute',
     top: 0,
     right: 0,
     display: 'flex',
@@ -37,26 +58,23 @@ const App = () => {
     transform: 'translate(-50%, -50%)',
     fontSize: '50px',
     backgroundColor: 'orange',
-    padding: '20px',
+    padding: '100px',
     borderRadius: '8px',
     textAlign: 'center',
+    alignItems: 'flex-start'
   }
 
   const tabStyles = {
-    padding: '10px',
     backgroundColor: 'rgb(204, 102, 0)',
     fontSize: '20px',
     color: 'black',
-    cursor: 'pointer',
-    marginRight: '10px',
+    fontWeight: 'bold',
+    marginRight: '5px',
+    padding: '15px',
+    borderRadius: '10px', // Optional: Add rounded corners to the content area 
   };
-    const tabNames = ["create new quotes", "about us"];
 
-    const contentStyles = {
-      marginTop: '50px', // Adjust as needed
-      fontSize: '18px',
-      color: 'black',
-    };
+    const tabNames = ["create new quotes", "about us"];
 
     const renderContent = () => {
       switch (activeTab) {
@@ -70,9 +88,9 @@ const App = () => {
     };
   
  
-
     return (
-      <div style={pageStyles}>
+      <div style={containerStyle}>
+      <div style={contentStyle}>
         <h1 style={headingStyles}>Gushpiknik-Quotes</h1>
         <div style={tabsContainerStyles}>
           {tabNames.map((tabName, index) => (
@@ -86,6 +104,7 @@ const App = () => {
           ))}
         </div>
         {renderContent()}
+      </div>
       </div>
     );
   };
